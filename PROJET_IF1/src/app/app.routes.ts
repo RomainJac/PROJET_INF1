dd1c02ec2bb3ee1ff156b71a00c2966cc220805b
Romain <romainjacob012@gmail.com>
2025-01-27 16:29:25 +0100

    Updating routes

--- PROJET_IF1/src/app/app.routes.ts
+++ PROJET_IF1/src/app/app.routes.ts
@@ -1,23 +1,18 @@
-import { Routes } from '@angular/router';
-import { ItemSearchPageComponent } from './page/item-search-page/item-search-page.component';
-import { LoginPageComponent } from './page/login-page/login-page.component';
-import {ItemDetailsPageComponent} from "./page/item-details-page/item-details-page.component";
 import {Routes} from '@angular/router';
+import {ItemDetailsPageComponent} from "./page/item-details-page/item-details-page.component";
 import {ItemSearchPageComponent} from './page/item-search-page/item-search-page.component';
 import {LoginPageComponent} from './page/login-page/login-page.component';
 import {PrivacyPolicyPageComponent} from "./page/privacy-policy-page/privacy-policy-page.component";
 import {TermsPageComponent} from "./page/terms-page/terms-page.component";
 
 export const routes: Routes = [
-  { path: 'login', component: LoginPageComponent },
-  { path: 'items', component: ItemSearchPageComponent },
-  { path: 'items/:id', component: ItemDetailsPageComponent },
-  { path: '', redirectTo: '/items', pathMatch: 'full' },
-  { path: '**', redirectTo: '/items' }
   {path: 'login', component: LoginPageComponent},
   {path: 'items', component: ItemSearchPageComponent},
-  {path: '', redirectTo: '/items', pathMatch: 'full'},
+  {path: 'items/:id', component: ItemDetailsPageComponent},
   {path: 'privacy-policy', component: PrivacyPolicyPageComponent},
   {path: 'terms', component: TermsPageComponent},
-  {path: '**', redirectTo: '/items'}
+  {path: '', redirectTo: '/items', pathMatch: 'full'},
+  {path: '**', redirectTo: '/items'},
+
+
 ];
