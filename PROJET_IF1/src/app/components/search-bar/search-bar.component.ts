687c56b9b8a0e407d6d1bae9a8061d993ed47987
Romain <romainjacob012@gmail.com>
2025-01-27 16:48:30 +0100

    Updating header and footer and adding some css

--- PROJET_IF1/src/app/components/search-bar/search-bar.component.ts
+++ PROJET_IF1/src/app/components/search-bar/search-bar.component.ts
@@ -1,18 +1,23 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  imports: [
+    FormsModule
+  ],
+  styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent {
   @Input() placeholder: string = 'Rechercher...';
   @Output() search = new EventEmitter<string>();
 
+  inputValue: string = '';
+
   onSearch(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
     this.search.emit(inputElement.value);
-
   }
 }
