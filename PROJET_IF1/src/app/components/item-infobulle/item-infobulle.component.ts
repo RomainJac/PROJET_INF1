444ec80277a09865162f2a7f6c68841e7fc7ad23
Lucian <lisniclucian@gmail.com>
2025-02-04 10:31:51 +0100

    --wip-- [skip ci]

--- PROJET_IF1/src/app/components/item-infobulle/item-infobulle.component.ts
+++ PROJET_IF1/src/app/components/item-infobulle/item-infobulle.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, Input} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, Input, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Item} from "../../data/model/Item";
 import {
@@ -16,42 +16,60 @@ import {InfobulleConditionComponent} from "../infobulle-condition/infobulle-cond
   templateUrl: './item-infobulle.component.html',
   styleUrl: './item-infobulle.component.css'
 })
-export class ItemInfobulleComponent {
+export class ItemInfobulleComponent implements AfterViewInit {
   @Input() x: number = 0;
   @Input() y: number = 0;
   @Input() item?: Item;
 
-  private margin = 50;
+  @ViewChild('tooltip') tooltipRef!: ElementRef<HTMLElement>;
 
-  constructor() {}
+  private margin = 10;
 
-  private adjustTooltipPosition() {
-    setTimeout(() => {
-      const tooltip = document.querySelector('.infobulle') as HTMLElement;
-      if (!tooltip) return;
+  constructor(private cdr: ChangeDetectorRef) {
+  }
 
-      const rect = tooltip.getBoundingClientRect();
-      const viewportWidth = window.innerWidth;
-      const viewportHeight = window.innerHeight;
+  ngAfterViewInit() {
+    this.adjustTooltipPosition();
+  }
 
-      let newX = this.x;
-      let newY = this.y;
-
-      if (this.x + rect.width + this.margin > viewportWidth) {
-        newX = viewportWidth - rect.width - this.margin ;
-      }
-
-      if (this.y + rect.height + this.margin > viewportHeight) {
-        newY = viewportHeight - rect.height - this.margin;
-      }
-
-      this.x = Math.max(this.margin, newX);
-      this.y = Math.max(this.margin, newY);
-    }, 0);
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    requestAnimationFrame(() => this.adjustTooltipPosition());
   }
 
   @HostListener('window:resize')
   onResize() {
-    this.adjustTooltipPosition();
+    requestAnimationFrame(() => this.adjustTooltipPosition());
+
+  }
+
+  private adjustTooltipPosition() {
+    if (!this.tooltipRef) return;
+
+    const tooltip = this.tooltipRef.nativeElement;
+    const rect = tooltip.getBoundingClientRect();
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    const scrollX = window.scrollX;
+    const scrollY = window.scrollY;
+
+    let newX = this.x;
+    let newY = this.y;
+
+
+    if (newX + rect.width + this.margin > viewportWidth + scrollX) {
+      newX = this.x - rect.width - this.margin;
+    }
+
+    if (newY + rect.height + this.margin > viewportHeight + scrollY) {
+      newY = this.y - rect.height - this.margin;
+    }
+
+
+    this.x = Math.max(this.margin + scrollX, newX);
+    this.y = Math.max(this.margin + scrollY, newY);
+
+    this.cdr.detectChanges();
+
   }
 }
